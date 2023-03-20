6147eb010dfda49b5b0e6a28b7ce3a7f260a9af1
Avesh Verma <verma.avesh001@gmail.com>
2023-03-20 22:18:41 +0530

    added hyperparameter tuning for models

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,9 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+from src.logger import logging
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -20,12 +22,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            logging.info("Tuning {}".format(model))
+            gs = GridSearchCV(model, para, cv = 3)            
+            gs.fit(X_train, y_train)
+            model.set_params(** gs.best_params_)
             model.fit(X_train, y_train)
+
             y_train_pred =  model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
@@ -36,3 +44,4 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
     except Exception as e:
         raise CustomException(e, sys)
 
+ 
